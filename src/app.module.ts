fece5e47fb157766833ab2816df9062439077c54
杨晓勇 <yangjackyong@163.com>
2024-06-24 14:01:41 +0800

    提交爬虫代码

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SpiderModule } from './spider/spider.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [SpiderModule],
+  imports: [SpiderModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
