e0d7c5be8cf002166b78b08a4597448c65ea62f2
杨晓勇 <yangjackyong@163.com>
2024-06-24 13:33:29 +0800

    提交爬虫代码

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService extends PrismaClient implements OnModuleInit {
     constructor() {
         super();
+    }
+    async onModuleInit() {
+        await this.$connect(); // 在模块初始化时连接到数据库
+    }
+
+    async onApplicationShutdown() {
+        await this.$disconnect(); // 在应用程序关闭时断开与数据库的连接
     }
 }
